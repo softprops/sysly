67932aaeb0bbceb22248582c3793267400f0dd39
softprops <d.tangren@gmail.com>
2015-02-20 08:42:18 -0500

    just focus on udp sockets for now

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 #![feature(io)]
-#![feature(path)]
+//#![feature(path)]
+
+extern crate time;
 
 use std::old_io::net::udp::UdpSocket;
 use std::old_io::net::ip::{ Ipv4Addr, SocketAddr };
-use std::old_io::net::pipe::UnixStream;
+//use std::old_io::net::pipe::UnixStream;
 
 fn main() {
+  let msg = format!("{} hi from Rust", time::now().rfc3339());
+
   let addr = SocketAddr { ip: Ipv4Addr(127,0,0,1), port: 514 };    
   let client_addr = SocketAddr { ip: Ipv4Addr(0,0,0,0), port: 0 };
   let mut udp = match UdpSocket::bind(client_addr) {
     Err(e) => panic!("error binding to {} -- {}", addr, e),
     Ok(s) => s
   };
-  let path = Path::new("unix:///var/run/syslog");
-  let mut stream = match UnixStream::connect(&path) {
-    Err(e) => panic!("error connecting to path -- {}", e),
-    Ok(s) => s,
-  };
 
-  let stream_result = stream.write_line(
-    "66 <174>1 2012-07-22T00:06:26-00:00 somehost Rust console 2 Hi from Rust");
-  match stream_result {
-      Err(e) => panic!("err {}", e),
-      Ok(_) => println!("stream send result ok")
-  };
+  //let path = Path::new("unix:///var/run/syslog");
+  //let mut stream = match UnixStream::connect(&path) {
+  //  Err(e) => panic!("error connecting to path -- {}", e),
+  //  Ok(s) => s,
+  //};
 
-  let udp_result = udp.send_to(
-    &"66 <174>1 2012-07-22T00:06:26-00:00 somehost Rust console 2 Hi from Rust".as_bytes(),
-   addr);
+  //let stream_result = stream.write_line(
+  //  "2012-07-22T00:06:26-00:00 somehost Rust console 2 Hi from Rust");
+  //match stream_result {
+  //    Err(e) => panic!("err {}", e),
+  //    Ok(_) => println!("stream send result ok")
+  //};
+
+  let udp_result = udp.send_to(msg.as_bytes(), addr);
   match udp_result {
       Err(e) => panic!("err {}", e),
       Ok(_) => println!("upd send result ok")
