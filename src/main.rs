95c6ff35ee3b4d9d7fe498ed538cbcab2e58799b
softprops <d.tangren@gmail.com>
2015-02-23 22:55:48 -0500

    main updates

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![feature(old_io)]
 extern crate sysly;
 
-use sysly::Syslog;
+use sysly::{ Facility, Syslog };
 use std::old_io::net::ip::{ Ipv4Addr, SocketAddr };
 
 fn main() {
   let host = SocketAddr { ip: Ipv4Addr(127,0,0,1), port: 514 };    
-  let mut syslog = Syslog::udp(host);
+  let mut syslog = Syslog::udp(host).facility(Facility::LOCAL3).tag("test");
   match syslog.info("Hello syslog. I'm rust.") {
     Err(e) => panic!("error sending -- {}", e),
     Ok(_) => println!("sent")
